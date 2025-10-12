0d651ab0c645d4f816ee806141fd35e929cc3a2d
james-parky <72101291+james-parky@users.noreply.github.com>
2025-10-12 21:56:22 +0100

    main: container: refactor display creation with Conainer constructors

--- main/src/display.rs
+++ main/src/display.rs
@@ -17,6 +17,34 @@ enum Container<'a> {
     },
 }
 
+impl<'a> Container<'a> {
+    fn try_from_window(window: &'a core_graphics::Window, bounds: Bounds) -> crate::Result<Self> {
+        let mut w = Window::try_new(window, bounds)?;
+        w.init()?;
+        Ok(Self::Leaf(w))
+    }
+
+    fn try_from_windows(
+        windows: &'a [core_graphics::Window],
+        bounds: &[Bounds],
+    ) -> crate::Result<Self> {
+        let children: Vec<Container> = windows
+            .iter()
+            .zip(bounds.iter())
+            .map(|(w, &b)| {
+                let mut w = Window::try_new(&w, b)?;
+                w.init()?;
+                Ok(Container::Leaf(w))
+            })
+            .collect::<crate::Result<_>>()?;
+
+        Ok(Self::Split {
+            direction: Direction::default(),
+            children,
+        })
+    }
+}
+
 #[derive(Debug)]
 pub(crate) struct Display<'a> {
     id: u64,
@@ -27,38 +55,21 @@ pub(crate) struct Display<'a> {
 impl<'a> Display<'a> {
     pub(crate) fn try_new(display: &'a core_graphics::Display) -> crate::Result<Self> {
         let container = match display.windows.len() {
-            1 => {
-                let mut w = Window::try_new(&display.windows[0], display.bounds)?;
-                w.init()?;
-                Container::Leaf(w)
-            }
+            1 => Container::try_from_window(&display.windows[0], display.bounds)?,
             n => {
                 let widths = split_n(display.bounds.width, n);
                 let xs = xs_from_widths(display.bounds.x, &widths);
                 let bounds: Vec<Bounds> = widths
-                    .iter()
-                    .zip(xs.iter())
-                    .map(|(&width, &x)| Bounds {
+                    .into_iter()
+                    .zip(xs.into_iter())
+                    .map(|(width, x)| Bounds {
                         width,
                         x,
                         ..display.bounds
                     })
                     .collect();
 
-                let children = (0..n)
-                    .map(|i| match Window::try_new(&display.windows[i], bounds[i]) {
-                        Ok(mut window) => {
-                            window.init()?;
-                            Ok(window)
-                        }
-                        Err(e) => Err(e),
-                    })
-                    .collect::<crate::Result<Vec<Window>>>()?;
-
-                Container::Split {
-                    direction: Direction::default(),
-                    children: children.into_iter().map(Container::Leaf).collect(),
-                }
+                Container::try_from_windows(&display.windows, &bounds)?
             }
         };
 
